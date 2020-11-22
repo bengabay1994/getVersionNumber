bd50b3cca67809db2616a9104ca7d8b78ea50086
unknown <bengabay1994@gmail.com>
2020-11-22 17:09:24 +0200

    a

--- getVersion.bat
+++ getVersion.bat
@@ -1,1 +1,16 @@
-echo %date:~6,1%.%date:~7,3%%date:~3,2%.%date:~0,2%%time:~0,2%%time:~3,2%
+@ECHO OFF
+SET d1=%date:~6,1%
+SET d2=%date:~7,3%
+SET d3=%date:~3,2%
+SET d4=%date:~0,2%
+IF NOT "%d1%"=="2" (
+	SET b1=%date:~10,1%
+	SET b2=%date:~11,3%
+	SET b3=%date:~7,2%
+	SET b4=%date:~4,2%
+	@ECHO ON
+	ECHO %b1%.%b2%%b3%.%b4%%time:~0,2%%time:~3,2%
+) ELSE (
+	@ECHO ON
+	ECHO %d1%.%d2%%d3%.%d4%%time:~0,2%%time:~3,2%
+)
